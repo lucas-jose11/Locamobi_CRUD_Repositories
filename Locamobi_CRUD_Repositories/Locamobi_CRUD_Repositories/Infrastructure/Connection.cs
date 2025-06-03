4fe791b7d2cc1a9a879b441c3d11f3ab68d8e3a1
Lucas José <lucasjosesilva012@gmail.com>
2025-06-03 11:27:42 -0300

    Estrutura do CRUD dos Repositories

---
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Infrastructure/Connection.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace MeuPrimeiroCrud.Infrastructure
+{
+    public class Connection
+    {
+        protected string connectionString = "Server= ||| HOST ||| ;Database= ||| NOME DA DATABASE ||| ;User= ||| USER ||| ;Password= ||| SENHA ||| ;";
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public async Task<int> Execute(string sql, object obj)
+        {
+            using (MySqlConnection con = GetConnection())
+            {
+                return await con.ExecuteAsync(sql, obj);
+            }
+        }
+    }
+}
---
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuPrimeiroCrud
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        }
+    }
+}
