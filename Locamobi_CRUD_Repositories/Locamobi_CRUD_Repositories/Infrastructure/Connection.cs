105e183c1cc9e129e5659eea132a336a33901d33
Lucas José <lucasjosesilva012@gmail.com>
2025-06-03 22:35:08 -0300

    Realizado a conexão com o banco de dados do notebook do Lucas e feito a parte da interface que aparecerá no terminal

--- Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Infrastructure/Connection.cs
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Infrastructure/Connection.cs
@@ -5,7 +5,7 @@ namespace MeuPrimeiroCrud.Infrastructure
 {
     public class Connection
     {
-        protected string connectionString = "Server= ||| HOST ||| ;Database= ||| NOME DA DATABASE ||| ;User= ||| USER ||| ;Password= ||| SENHA ||| ;";
+        protected string connectionString = "Server=servidor_teste;Database=locamobi;User=root;Password=toor;";
 
         public MySqlConnection GetConnection()
         {
--- Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Program.cs
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Program.cs
@@ -5,6 +5,72 @@
         static async Task Main(string[] args)
         {
 
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("CRUD da tabela Contrato do Banco de Dados Locamobi");
+                    Console.WriteLine("");
+                    Console.WriteLine("Escolha uma opção abaixo e escreva a letra correspondente:");
+                    Console.WriteLine("C - CREATE");
+                    Console.WriteLine("R - READ");
+                    Console.WriteLine("U - UPDATE");
+                    Console.WriteLine("D - DELETE");
+                    Console.WriteLine("");
+
+                    char op = Console.ReadLine().ToUpper()[0];
+
+                    switch (op)
+                    {
+                        case "C":
+                            break;
+
+                        case "R":
+                            break;
+
+                        case "U":
+                            break;
+
+                        case "D":
+                            break;
+
+                        default:
+                            throw new Exception("Opção inválida")
+                            break;
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+                Console.WriteLine("Escolha uma opção válida.");
+            }
+           
+               
+            
+            }
+
+        static async Task Create()
+        {
+
         }
+
+        static async Task Read()
+        {
+
+        }
+
+        static async Task Update()
+        {
+
+        }
+
+        static async Task Delete()
+        {
+
+        }
+
     }
 }
