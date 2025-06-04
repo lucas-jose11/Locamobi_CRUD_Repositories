423b24751d6f8c89ab621f955be8a5932e6df75b
joaonnz <joaonnzeduardo@gmail.com>
2025-06-04 11:11:17 -0300

    interface program iniciada

--- Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Program.cs
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Program.cs
@@ -4,8 +4,39 @@
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-           
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Cadastro de Veiculo");
+                    Console.WriteLine("Comandos \n");
+                    Console.WriteLine("C - Create | R - Read | U - Update | D - Delete");
+                    char op = Convert.ToChar(Console.ReadLine());
+
+                    switch (op)
+                    {
+                        case 'C':
+                            break;
+                        case 'R':
+                            break;
+                        case 'U':
+                            break;
+                        case 'D':
+                            break;
+                        default:
+                            Console.WriteLine("Error:");
+                            break;
+                    }
+
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERRO: {ex}");
+            }
          
 
         }
