12cd023b0924939dad94b86fa508623f1816d442
Sergiomdsjr <sergiomdsjunior@gmail.com>
2025-06-04 08:49:46 -0300

    Atualização

--- Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/obj/Debug/net7.0/Locamobi_CRUD_Repositories.AssemblyInfo.cs
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/obj/Debug/net7.0/Locamobi_CRUD_Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Locamobi_CRUD_Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0fe3e3ba71160fe63c9313e67d712c9804d335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d59cbab115e2fab6d28b3edbab393bac4fbab95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Locamobi_CRUD_Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Locamobi_CRUD_Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
