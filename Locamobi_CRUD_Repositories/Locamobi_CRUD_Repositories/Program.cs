6483e458773b9fff867d127dec3c96e48893d3d9
Lucas José <lucasjosesilva012@gmail.com>
2025-06-03 23:04:40 -0300

    Melhorado o código da interface que aparecerá no terminal com funções e try catch colocado

--- Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Program.cs
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Program.cs
@@ -4,73 +4,86 @@
     {
         static async Task Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+                ChoseOption(Menu());
+            }
+        }
 
+
+        static char Menu()
+        {
+            Console.WriteLine(">>>>>CRUD da tabela Contrato do Banco de Dados Locamobi<<<<<");
+            Console.WriteLine("");
+            Console.WriteLine("Escolha uma opção abaixo e escreva a letra correspondente:");
+            Console.WriteLine("C - CREATE");
+            Console.WriteLine("R - READ");
+            Console.WriteLine("U - UPDATE");
+            Console.WriteLine("D - DELETE");
+
+            char op = Console.ReadLine().ToUpper()[0];
+            return op;
+        }
+
+
+        static void ChoseOption(char op)
+        {
             try
             {
-                while (true)
+                switch (op)
                 {
-                    Console.WriteLine("CRUD da tabela Contrato do Banco de Dados Locamobi");
-                    Console.WriteLine("");
-                    Console.WriteLine("Escolha uma opção abaixo e escreva a letra correspondente:");
-                    Console.WriteLine("C - CREATE");
-                    Console.WriteLine("R - READ");
-                    Console.WriteLine("U - UPDATE");
-                    Console.WriteLine("D - DELETE");
-                    Console.WriteLine("");
+                    case 'C':
+                        break;
 
-                    char op = Console.ReadLine().ToUpper()[0];
+                    case 'R':
+                        break;
 
-                    switch (op)
-                    {
-                        case "C":
-                            break;
+                    case 'U':
+                        break;
 
-                        case "R":
-                            break;
+                    case 'D':
+                        break;
 
-                        case "U":
-                            break;
-
-                        case "D":
-                            break;
-
-                        default:
-                            throw new Exception("Opção inválida")
-                            break;
-                    }
-
+                    default:
+                        throw new Exception("Opção inválida.");
                 }
-
             }
             catch (Exception ex)
             {
+                Console.WriteLine("\n--------------------------------------");
                 Console.WriteLine($"Erro: {ex.Message}");
                 Console.WriteLine("Escolha uma opção válida.");
+                Console.WriteLine("Aperte QUALQUER TECLA para continuar");
+                Console.WriteLine("--------------------------------------");
+                Console.ReadKey();
             }
-           
-               
-            
-            }
+        }
+
+
+
 
         static async Task Create()
         {
 
         }
 
+
         static async Task Read()
         {
 
         }
+
 
         static async Task Update()
         {
 
         }
 
+
         static async Task Delete()
         {
 
         }
-
     }
 }
