9cc95050f69930053b70b8f05ab1d4eef24325f3
joaonnz <joaonnzeduardo@gmail.com>
2025-06-04 09:55:28 -0300

    IVeiculo e DTO estabelecidas

--- Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Contracts/Repository/IVeiculoRepository.cs
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/Contracts/Repository/IVeiculoRepository.cs
@@ -1,7 +1,20 @@
-﻿namespace Locamobi_CRUD_Repositories.Contracts.Repository
+﻿using Locamobi_CRUD_Repositories.DTO;
+using Locamobi_CRUD_Repositories.Entity;
+
+namespace Locamobi_CRUD_Repositories.Contracts.Repository
 {
     public interface IVeiculoRepository
     {
+        Task<IEnumerable<VeiculoEntity>> GetAll();
+
+        Task<VeiculoEntity> GetByCodVeiculo(int codVeiculo);
+
+        Task Insert(VeiculoInsertDTO veiculoInsert);
+
+        Task Update(VeiculoInsertDTO veiculoUpdate);
+
+        Task Delete(int codVeiculo);
+
 
     }
 }
--- Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/DTO/VeiculoInsertDTO.cs
+++ Locamobi_CRUD_Repositories/Locamobi_CRUD_Repositories/DTO/VeiculoInsertDTO.cs
@@ -2,6 +2,16 @@
 {
     public class VeiculoInsertDTO
     {
+        public int CODVEICULO { get; set; }
+        public string MODELO { get; set; }
+        public string MARCA { get; set; }
+        public int ANO { get; set; }
+        public int PLACA { get; set; }
+        public string COR { get; set; }
+        public int CIDADE_CODCID { get; set; }
+        public string CLASSIFIC { get; set; }
+        public string TIPO { get; set; }
+        public string USUARIO_CODUSER { get; set; }
 
     }
 }
